6897d163f43b130da9c3641bd595bdf00adaafb5
JoshDaos <joshuadaos1998@gmail.com>
2021-07-26 17:17:37 -0700

    worked on todolist

--- task-buddy-app/src/App.js
+++ task-buddy-app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="task-buddy">
       <h1>Task Buddy</h1>
-      <TodoForm />
+      <TodoList />
     </div>
   );
 }
